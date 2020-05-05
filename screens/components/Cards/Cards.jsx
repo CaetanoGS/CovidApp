4f1295f4815fe2e629f5fd870dd48fe79b76be1b
Gustavo  Caetano <gustavo@Gustavo.mshome.net>
2020-05-05 02:08:09 +0200

    Show Date in the card

--- screens/components/Cards/Cards.jsx
+++ screens/components/Cards/Cards.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
+import { Card } from 'react-native-elements';
 
-const Cards = (props) => {
-    console.log(props);
-    return(
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    
+    if(!confirmed){
+        return (
+            <Text>Loading ...</Text>
+        );
+    }
+
+    return (
+
         <View>
-            <Text>Cards</Text>
+
+            <Card>
+
+                <Text> { confirmed.value } </Text>
+                <Text> { new Date(lastUpdate).toDateString() } </Text>
+                <Text> Number of active cases of COVID-19</Text>
+
+            </Card>
+
+            <Card>
+
+                <Text>{ recovered.value }</Text>
+                <Text>{ new Date(lastUpdate).toDateString() }</Text>
+                <Text>Number of recoveries cases from COVID-19</Text>
+
+            </Card>
+
+            <Card>
+
+                <Text>{ deaths.value }</Text>
+                <Text>{ new Date(lastUpdate).toDateString() }</Text>
+                <Text>Number of deaths caused by COVID-19</Text>
+
+            </Card>
+
         </View>
+
     );
 }
 
