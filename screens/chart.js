8e0186069664ddbadd718d19e5b23d45da2e68b4
Gustavo  Caetano <gustavo@Gustavo.mshome.net>
2020-05-06 03:57:45 +0200

    Get Chart Better

--- screens/chart.js
+++ screens/chart.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView, SafeAreaView, View } from 'react-native';
-import { Chart } from './components';
+import { StyleSheet, View } from 'react-native';
 import { fetchDailyData } from './api';
-import { ECharts } from 'react-native-echarts-wrapper';
+
+import { ECharts } from "react-native-echarts-wrapper";
+
 
 
 
 export default class ChartPlot extends React.Component {
+
 
 
     state = {
@@ -23,23 +25,61 @@ export default class ChartPlot extends React.Component {
 
 
     option = {
-        xAxis: {
-            type: 'category',
-            data: [null]
+        title: {
+            text: 'Global Situation'
         },
-        yAxis: {
-            type: 'value',
-            data: [null]
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'cross',
+                label: {
+                    backgroundColor: '#6a7985'
+                }
+            }
         },
-        series: [{
-            type: 'line',
-            data: [null],
-            name: 'Confirmed Cases'
-        }, {
-            type: 'line',
-            data: [null],
-            name: 'Number of Deaths'
-        }]
+        legend: {
+            data: []
+        },
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                boundaryGap: false,
+                data: []
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: 'Deaths',
+                type: 'line',
+                stack: '总量',
+                areaStyle: {},
+                data: []
+            },
+            {
+                name: 'Infecteds',
+                type: 'line',
+                stack: '总量',
+                areaStyle: {},
+                data: [null]
+            }
+
+        ]
     };
 
 
@@ -47,28 +87,31 @@ export default class ChartPlot extends React.Component {
 
         const { data } = this.state;
 
-        this.option.xAxis.data = data.map((data) => data.date);
-        this.option.series[0].data = data.map((data) => data.confirmed);
-        this.option.series[1].data = data.map((data) => data.deaths);
+        const confirmed = data.map((data) => data.confirmed);
+        const deaths = data.map((data) => data.deaths);
+        const date = data.map((data) => data.date);
+
+        this.option.series[0].data = deaths;
+        this.option.series[1].data = confirmed;
+        this.option.legend.data = date;
+
+        console.log(this.option.series[0].data, this.option.series[1].data, this.option.legend.data);
 
 
         return (
             <View style={styles.chartContainer}>
-                <ECharts option={this.option}></ECharts>
+                <ECharts
+                    option={this.option}
+                    backgroundColor="rgba(250, 250, 250, 0.3)"
+                />
             </View>
         )
 
     }
 }
 
- ChartPlot;
-
 const styles = StyleSheet.create({
     chartContainer: {
-        flex: 1,
-        left: 5
-
-
-
+        flex: 1
     }
-})
+});
