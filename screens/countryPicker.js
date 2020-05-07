7b9f84c22945b4bab5ca0d312177b1630e24d4d3
Gustavo  Caetano <gustavo@Gustavo.mshome.net>
2020-05-07 02:15:30 +0200

    Get data with the select change

--- screens/countryPicker.js
+++ screens/countryPicker.js
@@ -1,7 +1,7 @@
 import React, { Component, Animated, useState, useEffect } from 'react'
 import { StyleSheet, View, Text, ScrollView, SafeAreaView } from 'react-native';
 import { Header } from 'react-native-elements';
-import { fetchCountries } from './api';
+import { fetchCountries, fetchCountriesData } from './api';
 import DropdownMenu from 'react-native-dropdown-menu';
 import { ECharts } from "react-native-echarts-wrapper";
 
@@ -11,7 +11,11 @@ import { ECharts } from "react-native-echarts-wrapper";
 class Country extends React.Component {
 
     state = {
-        countries: []
+        countries: [],
+        countriesData: {},
+        confirmed: null,
+        recovered: null,
+        deaths: null
     }
 
     async componentDidMount() {
@@ -22,17 +26,38 @@ class Country extends React.Component {
 
     }
 
+    handleCountryChange = async (country) => {
+
+        const fetchedCountriesData = await fetchCountriesData(country);
+
+        this.setState({ countriesData: fetchedCountriesData });
+        this.setState({ confirmed: fetchedCountriesData.confirmed.value });
+        this.setState({ recovered: fetchedCountriesData.recovered.value });
+        this.setState({ deaths: fetchedCountriesData.deaths.value });
+
+
+        //console.log(country);
+
+    }
+
     option = {
         xAxis: {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            data: [{
+                value: ['Confirmed'],
+            },{
+                value: ['Recovered'],
+            },{
+                value: ['Deaths']
+            }]
         },
         yAxis: {
             type: 'value'
         },
         series: [{
-            data: [120, 200, 150, 80, 70, 110, 130],
+            data: [],
             type: 'bar',
+            color: ['Dark', 'Yellow', 'Blue'],
             showBackground: true,
             backgroundStyle: {
                 color: 'rgba(220, 220, 220, 0.8)'
@@ -43,6 +68,20 @@ class Country extends React.Component {
     render() {
 
         var data = [this.state.countries];
+
+        var dataBar = [];
+
+        if(this.state.confirmed == null || this.state.recovered == null || this.state.recovered == null){ 
+            dataBar = [1, 2, 3];
+            this.option.series[0].data = dataBar;
+        }else{ 
+            dataBar.push(this.state.confirmed, this.state.recovered, this.state.deaths);
+            this.option.series[0].data = dataBar;
+        }
+
+        console.log(dataBar);
+
+
 
         if (data.length == 0) {
             return (
@@ -61,7 +100,7 @@ class Country extends React.Component {
                         activityTintColor={'green'}
                         data={data}
                         maxHeight={200} 
-                        handler={(selection, row) => this.setState({text: data[selection][row]})}
+                        handler={(selection, row) => this.handleCountryChange(data[selection][row])}
                     >
                     </DropdownMenu>
                 </ScrollView>
